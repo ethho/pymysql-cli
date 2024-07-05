c3806918c19ad4f6cf4de33932bc9f043cb7d30c
Ethan Ho <53266718+ethho@users.noreply.github.com>
2024-07-05 14:55:39 -0500

    MVP working

--- pymysql_cli/app.py
+++ pymysql_cli/app.py
@@ -1,12 +1,43 @@
 import typer
+import json
+from enum import Enum
 from typing_extensions import Annotated
 from rich.console import Console
 from rich.table import Table
-from .sql import run_sql
+from .sql import run_sql, SQLResult
 
 console = Console()
 
 app = typer.Typer()
+
+
+class Format(str, Enum):
+    table = "table"
+    yaml = "yaml"
+    json = "json"
+
+
+def render_table(resp: SQLResult):
+    table = Table()
+    if not resp:
+        table.add_column("No results")
+        console.print(table)
+        return
+    cols: list[str] = list(resp[0].keys())
+    for col in cols:
+        table.add_column(col)
+    for row in resp:
+        string_row = [str(row[col]) for col in cols]
+        table.add_row(*string_row)
+    console.print(table)
+
+
+def render_json(resp: SQLResult):
+    print(json.dumps(resp, indent=2, default=str))
+
+
+def render_yaml(resp: SQLResult):
+    raise NotImplementedError()
 
 
 @app.command()
@@ -15,14 +46,11 @@ def main(
     user_name: Annotated[str, typer.Argument(envvar="PYMYSQL_USER")],
     password: Annotated[str, typer.Argument(envvar="PYMYSQL_PASS")],
     sql: str,
-    format: Annotated[str, typer.Option("table", "--format")],
+    database: Annotated[str, typer.Option("--db", "-d")] = None,
+    format: Annotated[str, typer.Option("--format", "-f")] = "table",
 ):
     """
     Run a SQL query.
     """
-    print(f"{host=}")
-    print(f"{user_name=}")
-    print(f"{password=}")
-    print(f"{sql=}")
-    resp = run_sql(sql, host=host, user=user_name, password=password)
-    breakpoint()
+    resp = run_sql(sql, host=host, user=user_name, password=password, database=database)
+    globals()[f"render_{format}"](resp)
--- pymysql_cli/sql.py
+++ pymysql_cli/sql.py
@@ -1,19 +1,19 @@
 import pymysql.cursors
 import typer
-from typing import Dict, List, Any
+from typing import Dict, List, Any, Optional
 
 SQLResult = List[Dict[str, Any]]
 
 
 def get_connection(
-    host: str, user: str, password: str
+    host: str, user: str, password: str, database: Optional[str] = None
 ) -> pymysql.connections.Connection:
     try:
         conn = pymysql.connect(
             host=host,
             user=user,
             password=password,
-            # database="db",
+            database=database,
             # charset="utf8mb4",
             cursorclass=pymysql.cursors.DictCursor,
         )
@@ -27,6 +27,10 @@ def run_sql(sql: str, **config) -> SQLResult:
     connection = get_connection(**config)
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(sql)
-            result = cursor.fetchmany(size=config.get("page_size", 10_000))
-            return result
+            try:
+                cursor.execute(sql)
+                result = cursor.fetchmany(size=config.get("page_size", 10_000))
+                return result
+            except pymysql.err.OperationalError as e:
+                typer.Abort(f"Error: {e}")
+                raise
